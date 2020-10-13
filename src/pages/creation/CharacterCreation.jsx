717c398ab973ffa7fcbe203676dbec1011db2c7a
Daniel Ohn <ohndaniel@gmail.com>
2020-10-12 23:39:51 -0700

    Add basic structure for pages and components.

---
+++ src/pages/creation/CharacterCreation.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+const CharacaterCreation = () => {
+  return (
+    <div>
+      <h1>Character Creation</h1>
+      <p>This is the character creation page.</p>
+      <p>Steps to character creation:</p>
+      <ul>
+        <li>Ancestory</li>
+        <li>Heritage</li>
+        <li>Background</li>
+        <li>Class</li>
+        <li>Nationality</li>
+        <li>Deity</li>
+      </ul>
+    </div>
+  )
+}
+
+export default CharacaterCreation
