8f88905662c6abdb8bdd76e93efa9c8b935505ce
Daniel Ohn <ohndaniel@gmail.com>
2020-10-13 00:02:23 -0700

    Add Header to App.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>World of Nexus</h1>
+    </div>
+  )
+}
+
+export default Header
