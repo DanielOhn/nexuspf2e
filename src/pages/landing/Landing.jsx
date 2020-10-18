cb9fc1a31bc3ce648ccd07ea977c81cf8e76e2ea
Daniel Ohn <ohndaniel@gmail.com>
2020-10-17 17:08:36 -0700

    Fix CSS to make layout be centered of page.

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -2,7 +2,17 @@ import React from "react"
 import "./Landing.css"
 
 const Landing = () => {
-  return <h1>This is the landing page.</h1>
+  return (
+    <div className="landing">
+      <div className="heading">
+        <h2>This is the landing page.</h2>
+        <small>10/17/2020</small>
+      </div>
+      <div className="text">
+        <p>Ligma my Steve Jobs.</p>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
